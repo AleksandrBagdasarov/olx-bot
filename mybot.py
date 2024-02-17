c4d2d72b4200678a9b7195b3302e76daa2f7dee3
Oleksandr Bahdasarov <oleksandr.bahdasarov@TX-N-0984.local>
2024-02-18 00:46:03 +0100

    fixes

--- mybot.py
+++ mybot.py
@@ -1,32 +1,67 @@
 
-from telegram import Bot, InputFile
+from telegram import Bot, InputFile, InputMediaPhoto
 import requests
+import time
 # Replace 'YOUR_BOT_TOKEN' with your bot's token
 bot = Bot(token='6593653705:AAEPUt-mrRMDg-8ig8VIaSYVhf67hVh3-0I')
-
 # STATUS = "OFF"
 STATUS = "ON"
 
 # Function to send a message to the bot
 # Function to send a message to the bot
+import re
+
+def parse_message(message):
+    match = re.search(r'(\d+)\s*seconds', message)
+    if match:
+        return int(match.group(1))
+    else:
+        return None
+
 async def send_notification(message):
     if STATUS == "OFF":
         return
-    # Replace 'YOUR_CHAT_ID' with your chat ID
-    html = "<b>Notification</b>\n" + message
+    html = f"<b>{message}</b>"
     chat_id = '-221000265'
-    await bot.send_message(chat_id=chat_id, text=html, parse_mode='HTML')
+    await bot.send_message(chat_id=chat_id, text=html, parse_mode='HTML', read_timeout=10, write_timeout=10, connect_timeout=10)
 
-async def send_message_to_bot(obj):
+
+async def send_message_to_bot(
+        link: str,
+        title: str,
+        price: str,
+        imgs: list = None,
+        description: str = None,
+):
     if STATUS == "OFF":
         return
     # Replace 'YOUR_CHAT_ID' with your chat ID
     chat_id = '-221000265'
-    message = f"<a href='{obj['link']}'>{obj['title']}</a>\nPrice: {obj['price']}"
+    # write bold title as clickable link
+    # then add italy description
+    # then add bold price
     try:
-        response = requests.get(obj['img'])
-        photo = InputFile(response.content, filename='photo.jpg')
-        await bot.send_photo(chat_id=chat_id, photo=photo, caption=message, parse_mode='HTML')
+        message = f"<a href='{link}'><b>{title}</b></a>\n\n"
+        if description:
+            message += f"<i>{description}</i>\n\n"
+        message += f"<b>{price}</b>"
+
+        if not imgs:
+            await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML', read_timeout=10, write_timeout=10, connect_timeout=10)
+        elif len(imgs) == 1:
+            response = requests.get(imgs[0])
+            photo = InputFile(response.content, filename='photo1.jpg')
+            await bot.send_photo(chat_id=chat_id, photo=photo, caption=message, parse_mode='HTML', read_timeout=10, write_timeout=10, connect_timeout=10)
+        else:
+            media = []
+            for i, img in enumerate(imgs[:2]):
+                response = requests.get(img)
+                photo = InputMediaPhoto(response.content, filename=f'photo{i}.jpg')
+                media.append(photo)
+            await bot.send_media_group(chat_id=chat_id, media=media, caption=message, parse_mode='HTML', read_timeout=15, write_timeout=15, connect_timeout=15)
     except Exception as e:
-        print(f"Error: {e}")
-        await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
+        limit = parse_message(e)
+        if limit:
+            time.sleep(limit + 1)
+            await send_message_to_bot(link, title, price, imgs, description)
+            time.sleep(limit)
--- scraper.py
+++ scraper.py
@@ -1,39 +1,23 @@
-# To achieve this, you can use the Selenium WebDriver for Python along with BeautifulSoup for parsing the HTML. Here's a step-by-step plan:
-#
-# 1. Import the necessary libraries.
-# 2. Set up a headless Selenium WebDriver.
-# 3. Open the webpage.
-# 4. Wait until the page is fully loaded.
-# 5. Parse the page source with BeautifulSoup.
-# 6. Find all `<a>` tags that contain links similar to "href="/d/oferta/*".
-# 7. For each of these `<a>` tags, find the `<img>` tag with an `alt` attribute that is a part of `href`.
-# 8. Then find the `<h6>` tag that contains the same text as the `img alt` and next to `<h6>` the `<p>` tag that contains the text "za darmo".
-#
-# Here's the Python code that follows this plan:
-#
-# ```python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import re
 import sqlite3
 import asyncio
 from time import sleep
-from selenium.webdriver.common.keys import Keys
 import secrets
 
 def random_sleep():
     # sleep from 0.5 to 1.5 seconds
-    return secrets.randbelow(1000) / 1000
+    sleep((secrets.randbelow(1000) / 1000) + 0.5)
     # return 0
 # Create an event loop
 loop = asyncio.get_event_loop()
 
 from mybot import send_message_to_bot, send_notification
-conn = sqlite3.connect('scraper.db')
+conn = sqlite3.connect('db/scraper.db')
 c = conn.cursor()
 c.execute('''
     CREATE TABLE IF NOT EXISTS links (
@@ -41,7 +25,6 @@ c.execute('''
     )
 ''')
 
-loop.run_until_complete(send_notification("Starting the scraper..."))
 # Function to insert a link into the database
 def insert_link(url):
     try:
@@ -57,9 +40,12 @@ def link_exists(url):
     return c.fetchone() is not None
 
 
+new_links = []
+
+
 # Set up a headless Selenium WebDriver
 options = Options()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
@@ -68,85 +54,110 @@ options.add_argument("--disable-software-rasterizer")
 options.add_argument("--window-size=1920,1200")
 driver = webdriver.Chrome(options=options)
 
-# Open the webpage
-driver.get("https://www.olx.pl/dom-ogrod/meble/bielsko-biala/q-oddam/?search%5Bdist%5D=10&search%5Border%5D=created_at:desc")
 
-# Wait until the page is fully loaded
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-sleep(3)
-# Find the button with the specific ID and click on it
-# button = driver.find_element_by_id("onetrust-accept-btn-handler")
-button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
-button.click()
+def parse_item(href):
+    try:
+        driver.get(f"https://www.olx.pl{href}")
+        # Wait until the page is fully loaded
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+        # Parse the page source with BeautifulSoup
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        images_container = soup.find('div', attrs={"class": "swiper-wrapper"})
+        image_cart = images_container.find_all('div', attrs={"class": "swiper-zoom-container"})
+        srcs = [img.find('img')['src'] for img in image_cart]
+        description_container = soup.find('div', attrs={'data-cy': 'ad_description'})
+        description = description_container.find('div').text
 
-# Parse the page source with BeautifulSoup
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-# elements = soup.select('[class^="listing-grid-container"]')
+        title_container = soup.find('div', attrs={'data-cy': 'ad_title'})
+        title = title_container.find('h4').text
 
-# Find all <a> tags that contain links similar to "href="/d/oferta/*"
-a_tags = soup.find_all('a', href=re.compile(r'/d/oferta/.*'))
-if len(a_tags) > 0:
-    loop.run_until_complete(send_notification("Found " + str(len(a_tags)) + " links"))
-else:
-    loop.run_until_complete(send_notification("No links found"))
-new_links = 0
-for i, a_tag in enumerate(a_tags):
-    # Find the body of the webpage
-    # body = driver.find_element_by_tag_name("body")
-    body = driver.find_element(By.TAG_NAME, "body")
+        # price_container = soup.find('div', attrs={'data-cy': re.compile('^ad-price')})
+        price = soup.find('h3').text
+        loop.run_until_complete(send_message_to_bot(
+            link=f"https://www.olx.pl{href}",
+            title=title,
+            price=price,
+            imgs=srcs,
+            description=description
+        ))
+    except Exception as e:
+        print(e)
 
-    # Scroll down by sending the ARROW_DOWN key
-    body.send_keys(Keys.ARROW_DOWN)
-    sleep(random_sleep())
-    # Print the current link being processed
-    body.send_keys(Keys.ARROW_DOWN)
 
-    print(f"Processing link {i+1}/{len(a_tags)}")
-    # Get href value
-    href = a_tag['href']
-    alt_text = href.replace("/d/oferta/",  "").replace("-", " ")
-    # Find the <img> tag with an `alt` attribute that is a part of `href`
-    img_tag = a_tag.find('img')
-    # Find the same <a> tag using Selenium
-    a_tag_selenium = driver.find_element(By.XPATH, f"//a[@href='{href}']")
+def parse_page(url):
+    hrefs = []
+    driver.get(url)
 
-    # Scroll to the <a> tag
-    a_tag_selenium.location_once_scrolled_into_view
-    if not link_exists(href):
-        # Insert the link into the database
-        insert_link(href)
-        new_links += 1
+    # Wait until the page is fully loaded
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    sleep(3)
+    try:
+        # Find the button with the specific ID and click on it
+        # button = driver.find_element_by_id("onetrust-accept-btn-handler")
+        button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+        button.click()
+    except:
+        pass
+    # Parse the page source with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    orders = soup.find_all('div', attrs={'data-cy': 'l-card'})
+    total_orders = len(orders)
+    for i, order in enumerate(orders):
+        order_id = order['id']
+        if link_exists(order_id):
+            continue
+        insert_link(order_id)
+        new_links.append(1)
 
-        if img_tag:
-            try:
-                # Find the <h6> tag that contains the same text as the `img alt`
-                h6_tag = a_tag.find('h6')
+        a = order.find('a')
+        href = a['href']
+        title = order.find('h6')
+        a_tag_selenium = driver.find_element(By.XPATH, f"//a[@href='{href}']")
 
-                if h6_tag:
-                    # Find the <p> tag that contains the text "za darmo"
-                    p_tag = h6_tag.find_next_sibling('p')
+        # Scroll to the <a> tag
+        a_tag_selenium.location_once_scrolled_into_view
+        if a and title:
+            # parse_item(driver, href)
+            print(f"Processing link {i+1}/{total_orders}")
+            hrefs.append(href)
 
-                    if p_tag:
-                        print(f"href: {href}, h6 text: {h6_tag.text}, p text: {p_tag.text}")
-                        obj = {
-                            "link": f"https://www.olx.pl{href}",
-                            "img": img_tag['src'],
-                            "title": h6_tag.text,
-                            "price": p_tag.text
-                        }
-                        # Send the message to the bot
-                        loop.run_until_complete(send_message_to_bot(obj))
-            except Exception as e:
-                print(e)
-    sleep(random_sleep())
-if not new_links:
-    loop.run_until_complete(send_notification("No new links found"))
-# Close the event loop when you're done
-loop.close()
-# Close the driver
-driver.quit()
-# Close the connection to the database
-conn.close()
-# ```
-#
-# Please replace `webdriver.Chrome(options=options)` with the appropriate WebDriver for your browser and make sure to have the correct WebDriver installed.
+        random_sleep()
+    return hrefs
+
+
+def main():
+    try:
+        urls = [
+            "https://www.olx.pl/dom-ogrod/meble/bielsko-biala/q-oddam/?search%5Border%5D=created_at:desc",
+            # "https://www.olx.pl/sport-hobby/rowery/rowery-gorskie/bielsko-biala/?search%5Bfilter_float_price%3Ato%5D=300",
+            # "https://www.olx.pl/oddam-za-darmo/bielsko-biala/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=free",
+        ]
+        for url in urls:
+            hrefs = parse_page(url)
+            # hrefs = set(['/d/oferta/rurka-do-szafy-na-wieszaki-CID628-IDYNbth.html', '/d/oferta/oddam-za-darmo-meble-CID628-IDYMWWQ.html', '/d/oferta/oddam-za-darmo-fotele-CID628-IDYMUZv.html', '/d/oferta/sofa-kanapa-z-funkcja-spania-CID628-IDYMBpR.html', '/d/oferta/szafa-dziecieca-oddam-CID628-IDYMrfy.html', '/d/oferta/zestaw-mebli-mebloscianka-kolor-machon-oddam-za-darmo-CID628-IDYMqVN.html', '/d/oferta/soda-rozkladana-oddam-rezerwacja-CID628-IDYmLpS.html', '/d/oferta/szafa-przesuwna-z-lustrami-CID628-IDYM2Ex.html', '/d/oferta/oddam-za-darmo-kanape-wersalke-CID628-IDYLd4e.html', '/d/oferta/oddam-fotel-za-darmo-CID628-IDUc1lw.html', '/d/oferta/sofa-oddam-za-darmo-CID628-IDYKzRX.html', '/d/oferta/lozeczko-dzieciece-CID628-IDYKyPh.html', '/d/oferta/mebloscianka-zestaw-mebli-CID628-IDYi5ki.html', '/d/oferta/ochraniacz-warkocz-do-lozeczka-o-dl-420cm-CID628-IDXSQ0W.html', '/d/oferta/oddam-amerykanke-CID628-IDYJo7J.html', '/d/oferta/mebloscianka-prl-CID628-IDYJ8oR.html', '/d/oferta/fotele-uzywane-gratis-CID628-IDYIdPv.html', '/d/oferta/wersalka-uzywana-gratis-CID628-IDYIdyh.html', '/d/oferta/komplet-wypoczynkowy-skorzany-z-funkcja-spania-i-schowkiem-CID628-IDYG91v.html', '/d/oferta/fotel-gleboki-zamszowy-ciemny-CID628-IDTT0F6.html', '/d/oferta/oddam-za-darmo-lustro-z-szafka-za-czekolade-milka-CID628-IDYDLxf.html', '/d/oferta/oddam-za-darmo-mebloscianke-CID628-IDYBGTH.html', '/d/oferta/meble-do-salonu-jasnobrazowe-CID628-IDOtIka.html', '/d/oferta/sofa-fotel-oddam-za-darmo-CID628-IDYziyb.html', '/d/oferta/naroznik-rozkladany-za-darmo-CID628-IDT1eye.html', '/d/oferta/kanapa-wersalka-oddam-za-darmo-CID628-IDYvf6z.html', '/d/oferta/kanapa-rozkladana-CID628-IDXD9fL.html', '/d/oferta/kanapa-rozkladana-CID628-IDXCZ2y.html', '/d/oferta/stol-plus-krzesla-CID628-IDXj4Ug.html', '/d/oferta/dwie-sofy-oddam-za-darmo-stan-dobry-CID628-IDYqcUU.html', '/d/oferta/kanapa-rozkladana-CID628-IDYp7aQ.html', '/d/oferta/lozko-tapczan-z-kabina-na-posciel-CID628-IDVZVZS.html', '/d/oferta/stara-szafa-narzedziowa-CID628-IDM8bdR.html', '/d/oferta/oddam-za-darmo-szafa-2-drzwiowa-prl-wysoki-polysk-CID628-IDYzORP.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-meble-CID628-IDYMOA8.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-fotel-pojedynczy-i-podwojna-sofe-CID628-IDVoPof.html?reason=extended_search_extended_distance', '/d/oferta/wersalka-do-oddania-CID628-IDYtaLj.html?reason=extended_search_extended_distance', '/d/oferta/biurko-male-czarne-oddam-bielsko-biala-CID628-IDYIk7j.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-stol-CID628-IDYvaLT.html?reason=extended_search_extended_distance', '/d/oferta/do-oddania-za-darmo-szafa-CID628-IDYsUIS.html?reason=extended_search_extended_distance', '/d/oferta/lozko-oddam-za-darmo-aktualne-CID628-IDYsUB7.html?reason=extended_search_extended_distance', '/d/oferta/oddam-meble-za-symboliczne-150-pln-negocjuj-CID628-IDXF9hZ.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-stol-rozkladany-CID628-IDYABhs.html?reason=extended_search_extended_distance', '/d/oferta/sofa-tapczan-kanapa-wersalka-oddam-CID628-IDYs3w5.html?reason=extended_search_extended_distance', '/d/oferta/oddam-lozeczko-120x60-CID628-IDYMxHw.html?reason=extended_search_extended_distance', '/d/oferta/kanapa-oddam-za-darmo-CID628-IDYINvL.html?reason=extended_search_extended_distance', '/d/oferta/oddam-szafe-za-darmo-CID628-IDYIMTk.html?reason=extended_search_extended_distance', '/d/oferta/oddam-lozko-za-darmo-CID628-IDYIMOj.html?reason=extended_search_extended_distance', '/d/oferta/meble-do-oddania-CID628-IDYIEMr.html?reason=extended_search_extended_distance', '/d/oferta/sofa-skorzana-oddam-za-kawe-CID628-IDYxZr4.html?reason=extended_search_extended_distance', '/d/oferta/oddam-rozkladana-kanape-CID628-IDYpUHf.html?reason=extended_search_extended_distance', '/d/oferta/meble-uzywane-za-darmo-oddam-CID628-IDYLpz4.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-naroznik-szary-CID628-IDYnM4n.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-meble-drewniane-robione-na-zamowienie-CID628-IDY7IR9.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-szafe-CID628-IDX3RBx.html?reason=extended_search_extended_distance', '/d/oferta/oddam-kredens-za-darmo-CID628-IDX3RGs.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-szafe-CID628-IDX0MXv.html?reason=extended_search_extended_distance', '/d/oferta/mebloscianka-oddam-za-darmo-CID628-IDYELNa.html?reason=extended_search_extended_distance', '/d/oferta/stolik-oddam-za-darmo-CID628-IDYEIRl.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-segment-nrd-CID628-IDYvAVe.html?reason=extended_search_extended_distance', '/d/oferta/oddam-duza-szafa-drewniana-vintage-retro-polki-wieszaki-na-dzialke-CID628-IDYBYWA.html?reason=extended_search_extended_distance', '/d/oferta/fotel-oddam-za-darmo-CID628-IDYxq1j.html?reason=extended_search_extended_distance', '/d/oferta/oddam-w-dobre-rece-mebloscianke-politura-CID628-IDXZK1H.html?reason=extended_search_extended_distance', '/d/oferta/oddam-lozko-rozkladane-CID628-IDW9AlC.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-mebloscianka-CID628-IDYIqHE.html?reason=extended_search_extended_distance', '/d/oferta/kanapa-ikea-oddam-CID628-IDYz5WF.html?reason=extended_search_extended_distance', '/d/oferta/oddam-biurko-rogowe-CID628-IDYMlbd.html?reason=extended_search_extended_distance', '/d/oferta/oddam-materac-180x220-CID628-IDYLbcd.html?reason=extended_search_extended_distance', '/d/oferta/uzywane-krzeslo-oddam-za-darmo-CID628-IDYx1VT.html?reason=extended_search_extended_distance', '/d/oferta/oddam-biurko-niebieskie-CID628-IDYrUEi.html?reason=extended_search_extended_distance', '/d/oferta/oddam-za-darmo-tapczan-CID628-IDYFBbq.html?reason=extended_search_extended_distance', '/d/oferta/pilnie-oddam-za-darmo-meble-najpozniej-do-23-lutego-CID628-IDYL7UT.html?reason=extended_search_extended_distance', '/d/oferta/oddam-polki-dla-dziecka-za-dwa-duze-reczniki-papierowe-CID628-IDYJlVk.html?reason=extended_search_extended_distance'])
+            print(hrefs)
+            for href in hrefs:
+                parse_item(href)
+        driver.quit()
+        if not new_links:
+            loop.run_until_complete(send_notification("No new items found."))
+        conn.close()
+        loop.close()
+    except Exception as e:
+        print(e)
+    try:
+        driver.quit()
+    except:
+        pass
+    try:
+        conn.close()
+    except:
+        pass
+    try:
+        loop.close()
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    main()
